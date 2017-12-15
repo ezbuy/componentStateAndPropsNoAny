af3923d6b17b15b8e8e62e614b7bb311a9d4d073
王麟 <wanglin@ezbuy.com>
2017-12-15 13:55:19 +0800

    Fix no visit import super call

--- src/componentStateAndPropsNoAnyRule.ts
+++ src/componentStateAndPropsNoAnyRule.ts
@@ -24,6 +24,7 @@ class NoAnyWalker extends Lint.RuleWalker {
         if (importModule === "react") {
             this.hasImportedReact = true;
         }
+        super.visitImportDeclaration(node);
     }
 
     public visitClassDeclaration(node: ts.ClassDeclaration) {
